c770beeec58d887e796ffbc573977f00d55feaf5
WilliamDann <s-william.dann@lwtech.edu>
2020-12-17 14:02:20 -0800

    removed compiled js

--- Globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.REPAIR_WORKERS = exports.BUILD_WORKERS = exports.UPGRADE_WORKERS = exports.SPAWN_WORKERS = exports.MINE_WORKERS = void 0;
-exports.MINE_WORKERS = 3;
-exports.SPAWN_WORKERS = 1;
-exports.UPGRADE_WORKERS = 1;
-exports.BUILD_WORKERS = 1;
-exports.REPAIR_WORKERS = 1;
